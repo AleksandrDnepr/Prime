eb0b319c383769ebfe2def24bce51404c3142d7e
Yuliia Pakhachuk <y.pakhachuk@godeltech.com>
2022-09-21 17:39:13 +0200

    created paginationcomponent

---
+++ pagination/pagination.jsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+export default class Pagination {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return <div></div>
+    }
+}
