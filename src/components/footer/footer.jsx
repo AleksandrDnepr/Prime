e3d8d6e0595b59302d16a65246b6a8aca4eab640
Vira Smulska <vera.smulskaya@gmail.com>
2022-09-22 19:59:12 +0200

    create Logo component

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,21 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Logo from '../logo/logo';
+import "./footer.css";
+
+
 
 export default class Footer extends Component {
+
+
   render() {
     return (
       <footer className='footer'>
-        <div className='footer_logo'>
-          <img src="./logo.svg" alt="logo"></img>
+        <div className='footer__logo'>
+          <Logo color="#DF434A"/>
         </div>
-        <div className='footer_contact-info contact-info'>
-            <h3 className='contact-info_title'>Contact Info</h3>
-            <ul className='contact-info_list'>
-              <li className='contact-info_address'>
-                <img src='./icon-address' alt='icon-address'></img>
-                <p>24th Street, New York, USA</p>
+        <div className='footer__contact-info contact-info'>
+            <h3 className='contact-info__title'>Contact Info</h3>
+            <ul className='contact-info__list'>
+              <li className='contact-info__address'>
+                <img src='./icon-address.svg' alt='icon-address'></img>
+                <span>24th Street, New York, USA</span>
               </li>
-              <li className='contact-info_phone'></li>
-              <li className='contact-info_email'></li>
+              <li className='contact-info__phone'></li>
+                <img src='./icon-phone.svg' alt='icon-address'></img>
+                <span>24th Street, New York, USA</span>
+              <li className='contact-info__email'></li>
+                <img src='./icon-email.svg' alt='icon-address'></img>
+                <span>24th Street, New York, USA</span>
             </ul>
         </div>
 
