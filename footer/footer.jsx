72596c6e365d8dcca52dd83dd09933ea2103f487
Vira Smulska <vera.smulskaya@gmail.com>
2022-09-22 14:30:40 +0200

    add ctructure of footer

--- footer/footer.jsx
+++ footer/footer.jsx
@@ -1,9 +1,25 @@
 import React, { Component } from 'react'
 
-export default class footer extends Component {
+export default class Footer extends Component {
   render() {
     return (
-      <div>footer</div>
+      <footer className='footer'>
+        <div className='footer_logo'>
+          <img src="./logo.svg" alt="logo"></img>
+        </div>
+        <div className='footer_contact-info contact-info'>
+            <h3 className='contact-info_title'>Contact Info</h3>
+            <ul className='contact-info_list'>
+              <li className='contact-info_address'>
+                <img src='./icon-address' alt='icon-address'></img>
+                <p>24th Street, New York, USA</p>
+              </li>
+              <li className='contact-info_phone'></li>
+              <li className='contact-info_email'></li>
+            </ul>
+        </div>
+
+      </footer>
     )
   }
 }
