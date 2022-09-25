45d9205d2eb61eef1ef52ea6e0bb0040b86b4c30
o.barashovets <o.barashovets@godeltech.com>
2022-09-26 01:32:37 +0200

    ViewModeToggle added

---
+++ src/components/ViewModeToggle/ViewModeToggle.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import grid from"./grid.svg"
+import list from"./list.svg"
+import "./ViewModeToggle.css"
+
+
+export default class ViewModeToggle extends Component {
+  render() {
+    
+    const modes = [
+        {url:grid, mode:"grid", className:"grid"},
+        {url:list, mode:"list", className:"list"}
+    ]
+    return (
+        <div className="view-mode">
+        <p>View Mode:</p>
+        {modes.map((el) => (
+            
+            <button 
+                className={el.className} 
+                mode={el.mode}
+                onClick={()=> console.log(this.props.mode) }
+                >
+                <img src={el.url} />
+
+            </button>)
+        )}
+    </div>
+
+    )
+  }
+}
