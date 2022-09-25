213ba525769fd0181b4e3f173eb2e9319bb11b02
Vira Smulska <vera.smulskaya@gmail.com>
2022-09-25 17:41:22 +0200

    add footer__container

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -9,44 +9,53 @@ export default class Footer extends Component {
   render() {
     return (
       <footer className="footer">
-        <div className="footer__logo">
-          <Logo color="#DF434A" />
-        </div>
-        <div className="footer__contact-info contact-info">
-          <h3 className="contact-info__title">Contact Info</h3>
-          <ul className="contact-info__list">
-            <li>
-              <a
-                href="https://goo.gl/maps/vc3YA9dvdSV9W8bW6"
-                target="_blank"
-                rel="noreferrer"
-                className="contact-info__item"
-              >
-                <div className="contact-info__border">
-                  <img src={icon_address} alt="icon-address" width="16px"></img>
-                </div>
-                <div className="contact-info__text">
-                  24th Street, New York, USA
-                </div>
-              </a>
-            </li>
-            <li>
-              <a href="tel:+01234567890" className="contact-info__item">
-                <div className="contact-info__border">
-                  <img src={icon_phone} alt="icon-phone" width="16px"></img>
-                </div>
-                <div className="contact-info__text">+0 123-456-7890</div>
-              </a>
-            </li>
-            <li className="contact-info__item">
-              <a href="mailto:info@example.com" className="contact-info__item">
-                <div className="contact-info__border">
-                  <img src={icon_email} alt="icon-email" width="16px"></img>
-                </div>
-                <div className="contact-info__text">info@example.com</div>
-              </a>
-            </li>
-          </ul>
+        <div className="footer__container">
+          <div className="footer__logo">
+            <Logo color="#DF434A" />
+          </div>
+          <div className="footer__contact-info contact-info">
+            <h3 className="contact-info__title">Contact Info</h3>
+            <ul className="contact-info__list">
+              <li>
+                <a
+                  href="https://goo.gl/maps/vc3YA9dvdSV9W8bW6"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="contact-info__item"
+                >
+                  <div className="contact-info__border">
+                    <img
+                      src={icon_address}
+                      alt="icon-address"
+                      width="16px"
+                    ></img>
+                  </div>
+                  <div className="contact-info__text">
+                    24th Street, New York, USA
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a href="tel:+01234567890" className="contact-info__item">
+                  <div className="contact-info__border">
+                    <img src={icon_phone} alt="icon-phone" width="16px"></img>
+                  </div>
+                  <div className="contact-info__text">+0 123-456-7890</div>
+                </a>
+              </li>
+              <li className="contact-info__item">
+                <a
+                  href="mailto:info@example.com"
+                  className="contact-info__item"
+                >
+                  <div className="contact-info__border">
+                    <img src={icon_email} alt="icon-email" width="16px"></img>
+                  </div>
+                  <div className="contact-info__text">info@example.com</div>
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </footer>
     );
