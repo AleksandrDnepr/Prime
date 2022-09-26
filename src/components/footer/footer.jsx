f0dbffd4287525cfe49603ede12bc3ddeb049bb1
Vira Smulska <vera.smulskaya@gmail.com>
2022-09-26 22:12:33 +0200

    correction Logo component and some CSS

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -10,52 +10,55 @@ export default class Footer extends Component {
     return (
       <footer className="footer">
         <div className="footer__container">
-          <div className="footer__logo">
-            <Logo color="#DF434A" />
-          </div>
-          <div className="footer__contact-info contact-info">
+            <Logo color="red"/>         
+          <section className="footer__contact-info contact-info">
             <h3 className="contact-info__title">Contact Info</h3>
             <ul className="contact-info__list">
-              <li>
+              <li className="contact-info__text">
                 <a
                   href="https://goo.gl/maps/vc3YA9dvdSV9W8bW6"
                   target="_blank"
                   rel="noreferrer"
                   className="contact-info__item"
                 >
-                  <div className="contact-info__border">
-                    <img
-                      src={icon_address}
-                      alt="icon-address"
-                      width="16px"
-                    ></img>
-                  </div>
-                  <div className="contact-info__text">
-                    24th Street, New York, USA
-                  </div>
+                  <img
+                    className="contact-info__icon"
+                    src={icon_address}
+                    alt="icon-address"
+                    width="16px"
+                  ></img>
+                  <span>24th Street, New York, USA</span>
                 </a>
               </li>
-              <li>
+              <li className="contact-info__text">
                 <a href="tel:+01234567890" className="contact-info__item">
-                  <div className="contact-info__border">
-                    <img src={icon_phone} alt="icon-phone" width="16px"></img>
-                  </div>
-                  <div className="contact-info__text">+0 123-456-7890</div>
+                  <img
+                    className="contact-info__icon"
+                    src={icon_phone}
+                    alt="icon-phone"
+                    width="16px"
+                  ></img>
+
+                  <span>+0 123-456-7890</span>
                 </a>
               </li>
-              <li className="contact-info__item">
+              <li className="contact-info__text">
                 <a
                   href="mailto:info@example.com"
                   className="contact-info__item"
                 >
-                  <div className="contact-info__border">
-                    <img src={icon_email} alt="icon-email" width="16px"></img>
-                  </div>
-                  <div className="contact-info__text">info@example.com</div>
+                  <img
+                    className="contact-info__icon"
+                    src={icon_email}
+                    alt="icon-email"
+                    width="16px"
+                  ></img>
+
+                  <span>info@example.com</span>
                 </a>
               </li>
             </ul>
-          </div>
+          </section>
         </div>
       </footer>
     );
