8b5b50008d953d1edf350e54e90df20094423b81
Vira Smulska <vera.smulskaya@gmail.com>
2022-09-27 11:01:10 +0200

    fix conflicts in gallery

--- src/pages/components-gallery.jsx
+++ src/pages/components-gallery.jsx
@@ -1,25 +1,21 @@
 import { Component } from "react";
 
-<<<<<<< HEAD
 import Pagination from "../components/pagination/pagination";
-=======
+
 import Footer from "../components/footer/footer";
->>>>>>> add Footer in CompoComponentsGallery
 
 export default class ComponentsGallery extends Component {
+  render() {
+    return (
+      <div>
+        <Pagination
+          pages={15}
+          page={4}
+          onChange={(page) => console.log(`Page changed to ${page}`)}
+        />
 
-    render() {
-        return <div>
-
-
-<<<<<<< HEAD
-            <Pagination pages={15} page={4} onChange={page => console.log(`Page changed to ${page}`)}/>
-=======
-         <Footer />
-
-            
->>>>>>> add Footer in CompoComponentsGallery
-        
-        </div>
-    }
+        <Footer />
+      </div>
+    );
+  }
 }
