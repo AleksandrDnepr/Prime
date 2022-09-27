4f452f31498500f8eee8ca8eadb26a0572d4916e
Vira Smulska <vera.smulskaya@gmail.com>
2022-09-27 16:08:47 +0200

    create components AmenityListemItem and AmenityList

---
+++ src/components/amenitylist/amenitylist.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class AmenityList extends Component {
+  render() {
+    return (
+      <div>AmenityList
+        <ul>
+          <li></li>
+        </ul>
+      </div>
+    )
+  }
+}
---
+++ src/components/amenitylistitem/amenitylistitem.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+
+export default class AmenityListItem extends Component {
+  render() {
+    return (
+      <div>AmenityListItem</div>)
+  }
+}
