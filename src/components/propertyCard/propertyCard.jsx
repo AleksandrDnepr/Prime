80119acdcf4f8541d1f0bcf115bf2847ce6bce93
o.barashovets <o.barashovets@godeltech.com>
2022-09-28 13:37:21 +0200

    propertytCard updated

--- src/components/propertyCard/propertyCard.jsx
+++ src/components/propertyCard/propertyCard.jsx
@@ -8,18 +8,22 @@ export default class PropertyCard extends Component {
     render() {
     const {mode,picture,deal,type,price,title,location,description,details} = this.props;
     return (
-      <div className="container">
-      <img src={mainPhoto} alt="main-photo" />
-      <section className="info">
-      <img src={building} alt="building" />
-      <h2>For Sale</h2>
-      <h1>{price}</h1>
-      </section>
+      <a className="container">
+        <section>
+          <img src={mainPhoto} alt="main-photo" />
+          <div className="info">
+            <img src={building} alt="building" />
+            <h2>For Sale</h2>
+            <h1>{price}</h1>
+          </div>
+        </section>
+          <section className='description'>
       <h3>Dream Apartment for Young Family</h3>
       <h5>Pasadena, California</h5>
       <p>Cras lobortis, diam et ultricies imperdiet, urna urna pharetra est, vitae euismod lectus dolor nec...</p>
       <Title />
-      </div>
+          </section>
+      </a>
     )
   }
 }
