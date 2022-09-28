a3045cbec5ca075af6e932a8b4412b69080c2048
ang <a.lebedieva@godeltech.com>
2022-09-28 17:50:32 +0200

    Styling changed

--- src/components/features/features.jsx
+++ src/components/features/features.jsx
@@ -5,35 +5,25 @@ import pool from "./Pool.svg";
 import fence from "./Fence.svg";
 
 export default class Features extends Component {
-    constructor(props) {
-        super(props);
-        this.props = props;
-    };
 
-    container(src, text) {
+  container(src, text) {
     return <li className="feature_container"> 
-          
-                <img 
-                className="features_img"
-                src={src}
-                alt="{text} icon"
-                width="40px" >
-                </img>
-                    {text}
-        </li>
+              <img 
+              className="features_img"
+              src={src}
+              alt="${text}' icon" />
+              {text}
+           </li>
     };
     
   render() {
     return (
 
-        <section className="features_main_container">
-            <p className='features_title'>{this.props.title}</p>
-            <ul  className="features_container">
-          {this.container(paw, "Pets Allowed")}
-           {this.container(pool, "Outdoor Pool")}
-           {this.container(fence, "850 Sq Ft Garden")}
-           </ul>
-        </section>
+      <ul className="features_main_container">
+        {this.container(paw, "Pets Allowed")}
+        {this.container(pool, "Outdoor Pool")}
+        {this.container(fence, "850 Sq Ft Garden")}
+      </ul>
     )
   }
 }
