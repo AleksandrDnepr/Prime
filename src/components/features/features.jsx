bbedfefada254ee10b0fc6b9c099d5ea5730cad2
ang <a.lebedieva@godeltech.com>
2022-09-30 14:27:04 +0200

    Feature

--- src/components/features/features.jsx
+++ src/components/features/features.jsx
@@ -12,7 +12,7 @@ export default class Features extends Component {
               <img 
               className="features_img"
               src={src}
-              alt = {`${text} icon`}/>
+              alt = {`${text} icon`} />
               {text}
            </li>
     };
