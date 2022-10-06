baa5c79bafc538daaefc80ef3983b7edd02309f8
Vira Smulska <vera.smulskaya@gmail.com>
2022-10-06 16:50:58 +0200

    test

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,17 +1,15 @@
 import React, { Component } from "react";
-import { Link, withRouter } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 import Header from "../components/header/header";
 import PropertyList from "../components/propertyList/propertyList";
 import Footer from "../components/footer/footer";
-import data from '../data.json';
+import data from "../data.json";
 
 class Index extends Component {
 
   render() {
 
 const { apartaments } = data;
-console.log(apartaments);
-const { images, type, title,location, description, details} = apartaments;
 
     return (
       <>
