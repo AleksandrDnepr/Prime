7df996904500764ec43b43daa4d38e09a13a4362
o.barashovets <o.barashovets@godeltech.com>
2022-10-06 13:16:47 +0200

    icons apartment or townhose were fixed tasks 33,34

--- src/components/title/title.jsx
+++ src/components/title/title.jsx
@@ -6,27 +6,37 @@ import { ReactComponent as ApartmentSvg } from "../priceLabel/Apartment.svg";
 
 export default class Title extends Component {
 
-chooseImage = (type) => {
-  switch (type) {
+  chooseImage = (type) => {
+    switch (type) {
       case "apartment":
-          return <ApartmentSvg className='type-building-icon' />;
+        return <ApartmentSvg className='type-building-icon' />;
       case "townhouse":
-          return <TownHouseSvg className='type-building-icon' />;
+        return <TownHouseSvg className='type-building-icon' />;
       default: return null;
+    }
   }
-}
+
+  apartOrHouse = (type) => {
+    switch (type) {
+      case "apartment":
+        return "Apartment";
+      case "townhouse":
+        return "Townhouse";
+      default: return null;
+    }
+  }
 
   render() {
-   const { details: { area, bedrooms, bathrooms }, id, location, name, type } = this.props; 
+    const { details: { area, bedrooms, bathrooms }, id, location, name, type } = this.props;
 
     return (
       <section className="title title__section">
         <h1 className="title__name">{name}</h1>
         <p className="title__location">{location[0]}, {location[1]}</p>
         <ul className="title__details details">
-            <li className="details__type-card"><TownHouseSvg />{this.chooseImage(type)} {type}</li>
-            <li><PropertyDetails details={{ area, bedrooms, bathrooms }} mode="list"/></li>
-            <li className="details__id-card">ID: {id}</li>
+          <li className="details__type-card">{this.chooseImage(type)} {this.apartOrHouse(type)}</li>
+          <li><PropertyDetails details={{ area, bedrooms, bathrooms }} mode="list" /></li>
+          <li className="details__id-card">ID: {id}</li>
         </ul>
       </section>
     )
