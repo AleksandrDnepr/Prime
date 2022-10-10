c05c92a8b4c5597e903380316f428759a081bd08
Yuliia Pakhachuk <y.pakhachuk@godeltech.com>
2022-10-10 19:39:57 +0200

    <PriceLabel/> has BEM-fide

--- src/components/priceLabel/priceLabel.jsx
+++ src/components/priceLabel/priceLabel.jsx
@@ -1,6 +1,8 @@
-import React, { Component } from 'react'
-import { ReactComponent as ApartmentSvg } from "./Apartment.svg"
-import { ReactComponent as TownHouseSvg } from "./townhouse.svg"
+import React, { Component } from 'react';
+import { ReactComponent as Apartment } from "./Apartment.svg";
+import { ReactComponent as Townhouse } from "./townhouse.svg";
+import "./priceLabel.css";
+
 export default class PriceLabel extends Component {
     numberFormat = (price) => {
         const numbFmt = price.toLocaleString('ru-RU');
@@ -9,9 +11,9 @@ export default class PriceLabel extends Component {
     chooseImage = (type) => {
         switch (type) {
             case "apartment":
-                return <ApartmentSvg className='type-building-icon' />;
+                return <Apartment className='price-label__icon_apartament' />;
             case "townhouse":
-                return <TownHouseSvg className='type-building-icon' />;
+                return <Townhouse className='price-label__icon_townhouse' />;
             default: return null;
         }
     }
@@ -27,12 +29,10 @@ export default class PriceLabel extends Component {
     render() {
         const { price, deal, type } = this.props
         return (
-            <div>
-                <div className="price__label-block">
-                    {this.chooseImage(type)}
-                    <p className='type-building'>{this.saleOrRent(deal)}</p>
-                    <p className='price'>{this.numberFormat(price)} $</p>
-                </div>
+            <div className="price-label">
+                {this.chooseImage(type)}
+                <p className='price-label__icon'>{this.saleOrRent(deal)}</p>
+                <p className='price-label__text'>{this.numberFormat(price)} $</p>
             </div>
         )
     }
