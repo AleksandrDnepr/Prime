45019e867854e038e42ca3fd95ec019ef9d3f69d
o.barashovets <o.barashovets@godeltech.com>
2022-10-11 17:55:44 +0200

    bem updated

--- src/components/propertyDetails/propertyDetails.jsx
+++ src/components/propertyDetails/propertyDetails.jsx
@@ -4,15 +4,14 @@ import { ReactComponent as AreaSvg } from "./area.svg"
 import { ReactComponent as BedsSvg } from "./beds.svg"
 import { ReactComponent as BathsSvg } from './baths.svg'
 
-
 export default class PropertyDetails extends Component {
     render() {
         const { details, mode } = this.props;
         return (
-            <ul className={`details-list_${mode}`}>
-                <li ><AreaSvg className="details-list-item" />{details.area}ft<sup>2</sup></li>
-                <li ><BedsSvg className="details-list-item" />{details.bedrooms}</li>
-                <li ><BathsSvg className="details-list-item" />{details.bathrooms}</li>
+            <ul className={`details details_${mode}`}>
+                <li ><AreaSvg className="details__item" />{details.area}ft<sup>2</sup></li>
+                <li ><BedsSvg className="details__item" />{details.bedrooms}</li>
+                <li ><BathsSvg className="details__item" />{details.bathrooms}</li>
             </ul>
         )
     }
