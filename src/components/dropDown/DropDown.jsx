aac48bb50c4f1c2702c5ec0d86def06fcf56e301
o.barashovets <o.barashovets@godeltech.com>
2022-10-12 23:07:47 +0200

    The 'DropDown' component was updated

--- src/components/dropDown/DropDown.jsx
+++ src/components/dropDown/DropDown.jsx
@@ -2,41 +2,41 @@ import React, { Component } from 'react'
 import Select from 'react-select'
 import "./dropDown.css"
 
-
-
-
-
 export default class DropDown extends Component {
-    state={
+    state = {
         currentProperty: "",
     };
 
-    
-  render() {
-    
-    const getValue = ()=> {
-        return this.state.currentProperty? options.find(c => c.value===this.state.currentProperty):""
+    render() {
+
+        const getValue = () => {
+            return this.state.currentProperty ? options.find(selected => selected.value === this.state.currentProperty) : ""
+        }
+
+        const changeValue = (selected,e) => {
+            if (e.action === 'clear') {
+                return this.setState({ currentProperty: '' })
+            }
+            return this.setState({ currentProperty: selected.value })
+        }
+
+        const options = [
+            { 'value': 'single', 'label': 'Single-family' },
+            { 'value': 'house', 'label': 'Townhouse' },
+            { 'value': 'apt', 'label': 'Apartment' },
+        ]
+
+        return (
+            <div className='dropdown-input'>
+                <Select
+                    placeholder="Type"
+                    onChange={changeValue}
+                    value={getValue()}
+                    options={options}
+                    isClearable
+                />
+            </div>
+
+        )
     }
-
-    const changeValue=(selected)=>{
-return this.setState({currentProperty:selected.value})
-    }
-
-    const options=[
-        { 'value': 'single', 'label': 'Single-family' },
-        { 'value': 'house', 'label': 'Townhouse' },
-        { 'value': 'apt', 'label': 'Apartment' },
-      ]
-
-    return (
-      <div className='dropdown-input'>
-        <Select 
-        placeholder="Type"
-        onChange={changeValue}
-        value={getValue()} 
-        options={options}/>
-      </div>
-
-    )
-  }
 }
