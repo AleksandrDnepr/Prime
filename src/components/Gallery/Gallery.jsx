df29d409906bbb256881a052c7871e3a6f810240
o.barashovets <o.barashovets@godeltech.com>
2022-10-14 10:35:25 +0200

    Some fixes were reverted on the pages

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -10,16 +10,16 @@ export default class Gallery extends Component {
     }
 
     showNext() {
-        if (this.state.currentPhoto > this.props.pictGalery.length - this.state.currentPhoto + 1) {
+        if(this.state.currentPhoto>this.props.pictGalery.length-this.state.currentPhoto+1){
             return;
         }
-        else {
-            this.setState(prev => ({ currentPhoto: prev.currentPhoto + 1 }))
+        else{
+        this.setState(prev => ({ currentPhoto: prev.currentPhoto + 1 }))
         }
     }
 
     showPrev() {
-        if (this.state.currentPhoto <= 0) {
+        if(this.state.currentPhoto<=0){
             return;
         }
         this.setState(prev => ({ currentPhoto: prev.currentPhoto - 1 }))
@@ -28,12 +28,12 @@ export default class Gallery extends Component {
     render() {
         const { pictGalery } = this.props
         const { currentPhoto } = this.state
-        if (pictGalery.length === 0) return null
-        return <div className="gallery container__gallery">
-            <LeftArrow onClick={() => this.showPrev()} className="gallery__arrow-left" />
-            <img className="gallery__img" src={pictGalery[currentPhoto]} alt="First view with the property" />
-            <RightArrow onClick={() => this.showNext()} className="gallery__arrow-right" />
-        </div>
-
+        return (
+            <div className="gallery container__gallery">
+                <LeftArrow onClick={() => this.showPrev()} className="gallery__arrow-left" />
+                <img className="gallery__img" src={pictGalery[currentPhoto]} alt="First view with the property" />
+                <RightArrow onClick={() => this.showNext()} className="gallery__arrow-right" />
+            </div>
+        )
     }
 }
--- src/components/floorPlans/floorPlans.jsx
+++ src/components/floorPlans/floorPlans.jsx
@@ -9,36 +9,36 @@ export default class FloorPlans extends Component {
     }
 
     changeFloor(choosenFloor) {
-        this.setState({ currentFloor: choosenFloor });
+        this.setState({currentFloor: choosenFloor});
     }
 
     render() {
         const { plans } = this.props;
         const { currentFloor } = this.state;
-        if (plans.length === 0) return null;
-        return <section className="floorplan__section">
+        return (
+        <section className="floorplan__section">
             <Subtitle>FLOOR PLANS</Subtitle>
             <div className="floorplan__frame">
                 <nav className="floorplan__toggle">
                     <ol className="floorplan__menu">
                         {plans.map(plan => {
                             return (
-                                <li
-                                    className={`floorplan__menu-item ${plan.name === currentFloor.name ? "currentFloor" : ""}`}
-                                    key={plan.name}
-                                    onClick={() => this.changeFloor(plan)}>
-                                    {plan.name}
-                                </li>)
-                        })
+                            <li 
+                            className={`floorplan__menu-item ${plan.name === currentFloor.name ? "currentFloor" : ""}`} 
+                            key={plan.name} 
+                            onClick={() => this.changeFloor(plan)}> 
+                                {plan.name} 
+                            </li>)})
                         }
                     </ol>
                 </nav>
-                <img
-                    src={this.state.currentFloor.url}
-                    alt={`${this.state.currentFloor.name}'s plan'`}
-                    className="floorplan__picture"
-                />
+                    <img 
+                        src={this.state.currentFloor.url} 
+                        alt={`${this.state.currentFloor.name}'s plan'`} 
+                        className="floorplan__picture"
+                    />
             </div>
         </section>
+        )
     }
 }
