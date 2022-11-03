54110af528801cdb4ee0f4a05607ef06f9747749
VERA <viraxyz@ukr.net>
2022-11-03 21:18:48 +0200

    Revert "serve admin-ui to server"
    This reverts commit 42d48e0d7e2b04b29971ebe0e9560985ff31c7df.

--- server/src/index.js
+++ server/src/index.js
@@ -5,12 +5,10 @@ const config = require('config');
 const PORT = config.get('port');
 
 express()
-    .use('/admin', express.static('admin'))
-    .use('/', express.static('static'))
+    .use(express.static('static'))
     .use(express.json())
     .use('/api', router)
     .use((req, res, next) => {
         res.sendFile(path.join(__dirname, "..", "static", "index.html"));
     })
     .listen(PORT, () => console.log(`Started on :${PORT}`));
-    
