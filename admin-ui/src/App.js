00f6f65433153fbc4eb1799e655f2f05661a2a36
Vira Smulska <vera.smulskaya@gmail.com>
2022-11-04 14:10:40 +0100

    add uikit to admin-ua

--- admin-ui/src/App.js
+++ admin-ui/src/App.js
@@ -1,8 +1,18 @@
+import "uikit/dist/css/uikit.css";
+import "uikit/dist/js/uikit.js";
+
 function App() {
   return (
     <div className="App">
+      <ul uk-accordion="true">
+        <li>
+          <a className="uk-accordion-title" href="#">acardion</a>
+          <div className="uk-accordion-content">open</div>
+        </li>
+      </ul>
+      <button className="uk-button uk-button-primary">click me</button>
       <header className="App-header">
-       <p>ADMIN UI</p>
+        <p>ADMIN UI</p>
       </header>
     </div>
   );
