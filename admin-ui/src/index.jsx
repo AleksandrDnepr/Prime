d63cb12c1d5715a75cbc21856c56a66cfbfac98b
Vira Smulska <vera.smulskaya@gmail.com>
2022-11-08 23:36:42 +0100

    create login page and add route to login page

---
+++ admin-ui/src/index.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Login } from "./pages/login";
+import { HashRouter, Switch, Route } from "react-router-dom";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <React.StrictMode>
+        <HashRouter basename="/admin">
+          <Switch>
+            <Route path="/login" component={Login} />
+          </Switch>
+        </HashRouter>
+      </React.StrictMode>
+    );
+  }
+}
+
+function main() {
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+
+  root.render(<App />);
+}
+
+main();
---
+++ admin-ui/src/pages/login.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import Button from "@mui/material/Button";
+
+export class Login extends Component {
+  render() {
+    return <Button variant="contained">Hello World</Button>;
+  }
+}
