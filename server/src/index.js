71564f5653116d2db1628a45c053ef01c3cbf67f
VERA <viraxyz@ukr.net>
2022-11-09 00:13:32 +0200

    add sign-in route to admin-ui

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = require('./routes/');
+const authRouter = require('./routes/auth');
 const path = require('path');
 const config = require('config');
 const PORT = config.get('port');
@@ -9,6 +10,7 @@ express()
     .use('/', express.static('static'))
     .use(express.json())
     .use('/api', router)
+    .use('/admin', authRouter)
     .use((req, res, next) => {
         res.sendFile(path.join(__dirname, "..", "static", "index.html"));
     })
