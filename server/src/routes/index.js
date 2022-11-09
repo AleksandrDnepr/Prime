170f09bdc0765bbfd76bb119ae03773616d1bda0
VERA <viraxyz@ukr.net>
2022-11-09 19:52:02 +0200

    add config with google secret

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,10 +2,11 @@ const { Router } = require('express');
 const status = require('./status.js');
 const agents = require("./agents.js");
 const properties = require('./properties.js');
+const authRouter = require('./auth.js');
 
 module.exports = Router()
     .get('/status', status)
     .use('/agents', agents)
     .use('/properties', properties)
-    
+    .use('/auth', authRouter)
 
