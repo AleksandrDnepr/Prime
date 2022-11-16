03d11895cd95d6e71a09e6c2065900d35ec770d3
Ilya Dovban <i.dovban@godeltech.com>
2022-11-16 02:43:21 +0100

    Added Google authentication

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,11 +1,12 @@
 const { Router } = require('express');
+const auth = require('./auth.js');
 const status = require('./status.js');
 const agents = require("./agents.js");
 const properties = require('./properties.js');
 
 module.exports = Router()
+    .use('/auth', auth)
     .get('/status', status)
     .use('/agents', agents)
     .use('/properties', properties)
-    
 
