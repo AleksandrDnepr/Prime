6aab1186542ac9db2d9354ae21efd9909c75951f
Vira Smulska <vera.smulskaya@gmail.com>
2022-11-30 18:21:35 +0100

    test change login.jsx

--- admin-ui/src/pages/login.jsx
+++ admin-ui/src/pages/login.jsx
@@ -1,31 +1,32 @@
-import { Component } from "react";
-import { Redirect } from 'react-router-dom';
-import { Button, Box, Link } from "@mui/material";
-import Background from './background.jpg'
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import Background from "./background.jpg";
 
 export class Login extends Component {
-
   render() {
     if (this.props.user.email) {
       return <Redirect to="/properties" />;
     }
 
-    return <Box component="div" sx={{
-      width: "calc(100vw)",
-      height: "calc(100vh)",
-        backgroundImage: `url(${Background})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        textAlign: 'center',
-        lineHeight: 30
-       }}>
-    
-      <Button variant="contained" size="lg">
-        <Link underline="hover" href="/api/auth" color="#ffffff">
-          Sign in with Google
-        </Link>
-      </Button>
-    </Box>
+    return (
+      <Box
+        component="div"
+        sx={{
+          width: `calc(100vw)`,
+          height: `calc(100vh)`,
+          backgroundImage: `url(${Background})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          textAlign: "center",
+          lineHeight: 30,
+        }}
+      >
+        <Button variant="contained" size="lg">
+          <a href="/api/auth">Sign in with Google</a>
+        </Button>
+      </Box>
+    );
   }
 }
-
