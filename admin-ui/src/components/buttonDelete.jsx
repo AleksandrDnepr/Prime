a3017f0ca22aa4822b8b43a636f6b0e33e0b83a2
Vira Smulska <vera.smulskaya@gmail.com>
2022-12-15 13:46:05 +0100

    add mui/icons-material and create two button  components

---
+++ admin-ui/src/components/buttonDelete.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Close from "@mui/icons-material/Close";
+
+export default class ButtonDelete extends Component {
+  render() {
+    return (
+      <div>
+        <Close />
+      </div>
+    );
+  }
+}
--- admin-ui/src/components/propertyItem.jsx
+++ admin-ui/src/components/propertyItem.jsx
@@ -1,5 +1,5 @@
 import { Component } from "react";
-import {Link as RouterLink} from 'react-router-dom'
+import { Link as RouterLink } from "react-router-dom";
 
 import {
   Avatar,
@@ -9,29 +9,38 @@ import {
   ListItemAvatar,
   ListItemText,
 } from "@mui/material";
+import ButtonEdit from "./buttonEdit";
+import ButtonDelete from "./buttonDelete";
 
 export class PropertyItem extends Component {
   render() {
     const { id, title, preview } = this.props;
 
     return (
-        <ListItem
-          alignItems="center"
-          sx={{
-            flexDirection:"column",
-            alignItems: "flex-start",
-            paddingTop: 0.5,
-            paddingBottom: 0.5,
-          }}
+      <ListItem
+        alignItems="center"
+        sx={{
+          flexDirection: "column",
+          alignItems: "flex-start",
+          paddingTop: 0.5,
+          paddingBottom: 0.5,
+        }}
+      >
+        <Link
+          underline="hover"
+          component={RouterLink}
+          to={`/properties/${id}/messages`}
+          sx={{ display: "flex" }}
         >
-          <Link  underline="hover" component={RouterLink} to={`/properties/${id}/messages`} sx={{display: "flex",}}>
-              <ListItemAvatar>
-                <Avatar variant="square" alt={title} src={preview} />
-              </ListItemAvatar>
-              <ListItemText primary={title} />
-          </Link>
-          <Divider sx={{width:"100%"}}/>
-        </ListItem>
+          <ListItemAvatar>
+            <Avatar variant="square" alt={title} src={preview} />
+          </ListItemAvatar>
+          <ListItemText primary={title} />
+          <ButtonEdit />
+          <ButtonDelete />
+        </Link>
+        <Divider sx={{ width: "100%" }} />
+      </ListItem>
     );
   }
 }
