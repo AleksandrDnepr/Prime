ec7bebfd0fd1e485b51cc089f038ebf6fbc39389
Vira Smulska <vera.smulskaya@gmail.com>
2022-12-15 13:46:05 +0100

    add mui/icons-material and create two button  components

---
+++ admin-ui/src/components/buttonEdit.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+import Edit from "@mui/icons-material/Edit";
+
+export default class ButtonEdit extends Component {
+  render() {
+    return (
+      <div>
+        <Edit />
+      </div>
+    );
+  }
+}
