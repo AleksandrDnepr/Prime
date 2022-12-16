fcc0aa780a0af732c812fffaa5d4c66ffb212d70
o.barashovets <o.barashovets@godeltech.com>
2022-12-16 14:37:57 +0100

    hashrouter import was moved

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,18 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import ComponentsGallery from "./pages/components-gallery";
-import { HashRouter, Switch, Route, BrowserRouter } from 'react-router-dom';
+import { Switch, Route, BrowserRouter } from "react-router-dom";
 import Index from "./pages/index.jsx";
 import Property from "./pages/property";
 import NotFoundError from "./components/notFoundError/notFoundError";
 
-
-
-export default class App extends React.Component  {
+export default class App extends React.Component {
   render() {
     return (
-      <React.StrictMode>  
-      
+      <React.StrictMode>
         <BrowserRouter basename=".">
           <Switch>
             <Route path="/component_gallery" component={ComponentsGallery} />
@@ -20,23 +17,22 @@ export default class App extends React.Component  {
             <Route path="/page_not_found" component={NotFoundError} />
 
             <Route path="/:property_id" component={Property} />
-          
-            <Route path="/" component={Index} />
 
-          </Switch>  
+            <Route path="/" component={Index} />
+          </Switch>
         </BrowserRouter>
       </React.StrictMode>
-    )
+    );
   }
 }
 
 async function main() {
-  const root = document.createElement('div');
-  const modalRoot = document.createElement('div');
-  modalRoot.id = 'modal-root';
-  document.querySelector('body').appendChild(root);
-  document.querySelector('body').appendChild(modalRoot);
-  document.querySelector('body').appendChild(modalRoot);
+  const root = document.createElement("div");
+  const modalRoot = document.createElement("div");
+  modalRoot.id = "modal-root";
+  document.querySelector("body").appendChild(root);
+  document.querySelector("body").appendChild(modalRoot);
+  document.querySelector("body").appendChild(modalRoot);
   ReactDOM.createRoot(root).render(<App />);
 }
 
