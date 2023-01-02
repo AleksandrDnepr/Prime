e9b6c48a8718924ee0589328df5f563bfb3a407a
Vira Smulska <vera.smulskaya@gmail.com>
2023-01-02 14:06:41 +0100

    commit before rebase

--- admin-ui/src/components/propertyForm.jsx
+++ admin-ui/src/components/propertyForm.jsx
@@ -2,12 +2,15 @@ import React, { Component } from "react";
 import {
   OutlinedInput,
   FormGroup,
+  FormControl,
   FormControlLabel,
   Checkbox,
   Select,
   MenuItem,
-  TextField, 
-  Stack
+  TextField,
+  Stack,
+  InputLabel,
+  Button
 } from "@mui/material";
 
 export class PropertyForm extends Component {
@@ -44,30 +47,39 @@ export class PropertyForm extends Component {
     } = this.state;
 
     return (
-      <Stack sx={{maxWidth: "1000px", margin: "auto"}} spacing={2}>
-        <OutlinedInput placeholder="Title">Title</OutlinedInput>
-        <OutlinedInput placeholder="Title">ID</OutlinedInput>
-        <OutlinedInput placeholder="Title">City</OutlinedInput>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={locationState}
-          label="State"
-          onChange={this.handleChange}
+      <Stack sx={{ maxWidth: "1000px", margin: "auto" }} spacing={2}>
+        <TextField label="Title" autoFocus />
+        <TextField label="ID"/>
+        <TextField label="City"/>
+        <FormControl>
+          <InputLabel>State</InputLabel>
+          <Select
+            id="locationState"
+            value={locationState}
+            label="locationState"
+            onChange={this.handleChange}
+          >
+            <MenuItem value={10}>California</MenuItem>
+            <MenuItem value={20}>Florida</MenuItem>
+          </Select>
+        </FormControl>
+        <FormGroup
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+          }}
         >
-          <MenuItem value={10}>California</MenuItem>
-          <MenuItem value={20}>Florida</MenuItem>
-        </Select>
-        <FormGroup sx={{display: "flex", flexDirection: "row", justifyContent: "center"}}>
           <FormControlLabel
             control={<Checkbox defaultChecked />}
             label="Rent"
           />
           <FormControlLabel control={<Checkbox />} label="Sale" />
         </FormGroup>
+        <FormControl>
+        <InputLabel>Type</InputLabel>
         <Select
-          labelId="Type"
-          id="demo-simple-select"
+          id="type"
           value={type}
           label="Type"
           onChange={this.handleChange}
@@ -75,14 +87,18 @@ export class PropertyForm extends Component {
           <MenuItem value={10}>Townhouse</MenuItem>
           <MenuItem value={20}>Apartment</MenuItem>
         </Select>
-        <OutlinedInput type="number" placeholder="Price">Price</OutlinedInput>
-        <OutlinedInput type="number" placeholder="Area">Area</OutlinedInput>
-        <OutlinedInput type="number" placeholder="Bedroom">Bedroom</OutlinedInput>
-        <OutlinedInput type="number" placeholder="Bathroom">Bathroom</OutlinedInput>
-        <OutlinedInput type="date" placeholder="Year">Year</OutlinedInput>
-        <TextField placeholder="Description"></TextField>
+        </FormControl>
+        <TextField label="Price" type="number" />
+        <TextField label="Area" type="number" />
+        <TextField label="Bedroom" type="number" />
+        <TextField label="Bathroom" type="number" />
+        <TextField label="Year" type="date" />
+        <TextField label="Decscription" />
+        <Stack sx={{ margin: "20px auto", justifyContent: "center", }} direction="row" spacing={2}>
+          <Button onClick={() => this.edit()} sx={{width: "80px"}}>Edit</Button>
+          <Button onClick={() => this.create()} sx={{width: "80px"}}>Create</Button>
+        </Stack>
       </Stack>
     );
   }
 }
-
