5c5266d11333eef1df1fa80d67237c24b4443782
VERA <viraxyz@ukr.net>
2023-01-02 21:22:42 +0200

    fix pathes in Tabs

--- admin-ui/src/components/tabsBlock.jsx
+++ admin-ui/src/components/tabsBlock.jsx
@@ -1,38 +1,50 @@
 import { Component } from "react";
-import {NavLink, Switch, Route} from "react-router-dom";
+import { NavLink, Switch, Route } from "react-router-dom";
 import { Box } from "@mui/material";
-import { Tab } from '@mui/material';
-import { Tabs } from '@mui/material';
+import { Tab } from "@mui/material";
+import { Tabs } from "@mui/material";
 
-export class TabsBlock extends Component { 
+export class TabsBlock extends Component {
   state = {
-    value: 4
-  }     
+    value: 0,
+  };
 
-  render() {    
-    const {value } = this.state
+  render() {
+    const { value } = this.state;
 
-    const tabs = [
-      {label: "images",path: "images" },
-      {label: "floor plans", path: "floor_plans" },
-      {label: "features", path: "features" },
-      {label: "amenities", path: "amenities" },
-      {label: "messages", path: "messages" },
-  ]
-  const switchTabs = (event, newValue) => {
-    this.setState({value: newValue})
-  }
+    const tabs = ["messages", "images", "floor plans", "features", "amenities"];
+
+    const switchTabs = (event, newValue) => {
+      this.setState({ value: newValue });
+    };
 
     return (
-      <Box sx={{ width: '100%' }}>
-      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs value={value} onChange={switchTabs}>{
-          tabs.map( (tab, index) => 
-          <Tab key={index} label={tab.label} component={NavLink} to={`/properties/:property_id/${tab.path}`}/>      
-          )
-        }
-        </Tabs>      
-      </Box>
+      <Box sx={{ width: "100%" }}>
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+          <Tabs value={value} onChange={switchTabs}>
+            {tabs.map((tab, index) => {
+              if (tab === "floor plans") {
+                return (
+                  <Tab
+                    key={index}
+                    label={tab}
+                    component={NavLink}
+                    to={`/properties/:property_id/floor_plans`}
+                  />
+                );
+              } else {
+                return (
+                  <Tab
+                    key={index}
+                    label={tab}
+                    component={NavLink}
+                    to={`/properties/:property_id/${tab}`}
+                  />
+                );
+              }
+            })}
+          </Tabs>
+        </Box>
       </Box>
     );
   }
