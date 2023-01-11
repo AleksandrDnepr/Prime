4a3c9c72dadcff2ee6eddea982c2892d10b9c84f
o.barashovets <o.barashovets@godeltech.com>
2023-01-11 02:38:55 +0100

    console log of inputed values

--- admin-ui/src/pages/createAgent.jsx
+++ admin-ui/src/pages/createAgent.jsx
@@ -5,14 +5,22 @@ import { withRouter } from "react-router-dom";
 import AgentForm from "../components/agentForm/agentForm.jsx";
 
 export class CreateAgent extends Component {
+  close() {
+    this.props.history.push("/agents");
+  }
   render() {
     const isOpened = this.props.match.path === "/agents/create";
     return (
       <ModalWindow open={isOpened}>
         <Typography variant="h6" gutterBottom>
-          Fill the form to create new agent:
+          Fill the form to create a new agent:
         </Typography>
-        <AgentForm />
+        <AgentForm
+          onClose={() => this.close()}
+          onSubmit={(values) => {
+            console.log(values);
+          }}
+        />
       </ModalWindow>
     );
   }
