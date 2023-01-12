3285c074d3e01cb0240fab4593d020a954e5f0d5
Anhelina Lebedieva <a.lebedieva@godeltech.com>
2023-01-12 12:54:30 +0100

    Data loaded

--- admin-ui/src/pages/editProperty.jsx
+++ admin-ui/src/pages/editProperty.jsx
@@ -1,15 +1,119 @@
 import { Component } from "react";
 import { PropertyForm } from "../components/propertyForm";
 import { ModalWindow } from "../components/modalWindow";
+import { withRouter } from "react-router-dom";
 
-export class EditProperty extends Component {
+class EditProperty extends Component {
+  // state = {
+  //     values: {
+  //       title: "1",
+  //       id: "1",
+  //       locationCity: "",
+  //       locationState: "",
+  //       deal: "",
+  //       type: "",
+  //       price: "",
+  //       area: "",
+  //       bedroom: "",
+  //       bathroom: "",
+  //       year: "",
+  //       description: "",
+  //     },
+  //   };
+
+  state = {
+    property: [],
+    isLoading: true,
+  };
+  close() {
+    this.props.history.push("/properties");
+  }
+
+  componentDidMount() {
+    this.ccc();
+
+    // fetch(`/api/properties/A0001/edit`)
+    // .then((res) => {
+    //         if (res.ok) {
+    //           res.json().then((data) => {
+    //             this.setState({ property: data });
+    //           })}
+    //         });
+    // .then((response) => console.log(response));
+  }
+
+  ccc() {
+    const { id } = this.props.match.params;
+
+    fetch(`/api/properties/${id}`)
+      .then((res) => {
+        if (res.ok) {
+          res.json().then((data) => {
+            this.setState({ property: data });
+            console.log(data);
+          });
+        } else {
+          this.setState({
+            error: `Property with id`,
+          });
+        }
+      })
+
+      .catch(() => this.setState({ error: "Something went wrong" }))
+      .finally(() => this.setState({ isLoading: false }));
+  }
+
+  // editProperty(values) {
+  //         const propId = parseInt(this.props.match.params.prop_id);
+  //     console.log(propId);
+  //         fetch(`/api/properties/${propId}`, {
+  //           method: "PATCH",
+  //           headers: {
+  //             "content-type": "application/json",
+  //           },
+  //           body: JSON.stringify({
+  //             ...values,
+  //           }),
+  //         })
+  //           .then((data) => data.json())
+  //           .then(({ success }) => {
+  //             if (success) {
+  //               this.props.onEdit();
+  //             }
+  //           })
+  //           .catch((error) => this.setState({ error }))
+  //           .finally(() => {
+  //             this.close();
+  //           });
+  //       }
+
   render() {
-    console.log("A");
-
+    const isOpened = this.props.match.path === "/properties/:id/edit";
+    //   const {prop_id} = this.props;
+    const { values } = this.state;
+    //   console.log(property);
     return (
-      <ModalWindow open={"/properties/:id/edit"}>
-        <PropertyForm />
+      <ModalWindow open={isOpened}>
+        <PropertyForm
+          // defaultValues = {property}
+          // defaultValues = {values}
+          // onSubmit={(values) => {
+          //     this.edit(values);}}
+          onClose={() => this.close()}
+          buttonName="Edit"
+        />
       </ModalWindow>
     );
   }
 }
+
+export default withRouter(EditProperty);
+
+//     method: "PUT",
+//     headers: {
+//       "content-type": "application/json",
+//     },
+//     body: JSON.stringify({
+//       ...property,
+//     }),
+//   })
