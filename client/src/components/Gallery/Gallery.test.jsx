650822a2ae0951d25db9cbaa0a7f01e9433b2584
Anhelina Lebedieva <a.lebedieva@godeltech.com>
2023-01-20 12:13:54 +0100

    Left, Right, Slider components

--- client/src/components/Gallery/Gallery.test.jsx
+++ client/src/components/Gallery/Gallery.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import { RightArrow } from "./rightArrow.jsx";
 import { LeftArrow } from "./leftArrow.jsx";
+import { GallerySlider } from "./gallerySlider.jsx";
 
 describe("Gallery", () => {
   it("renders", async () => {
@@ -28,5 +29,22 @@ describe("Gallery", () => {
     const leftArrowRender = render(<LeftArrow currentIndex={0} />);
 
     expect(leftArrowRender.container.firstChild).toBeNull();
+  });
+
+  it("renders", async () => {
+    const gallerySliderRender = render(
+      <GallerySlider
+        currentIndex={0}
+        pictures={["first", "second", "third", "fourth"]}
+      />
+    );
+    expect(
+      gallerySliderRender.container.firstChild.classList.contains(
+        "gallery__slider"
+      )
+    ).toBe(true);
+    expect(
+      screen.getAllByAltText(/Slider with apartment's images/i)[0]
+    ).toHaveClass("gallery__slider_img");
   });
 });
