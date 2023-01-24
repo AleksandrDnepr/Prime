057ab4947ce9ce5a7d7310871e04529354403124
o.barashovets <o.barashovets@godeltech.com>
2023-01-24 14:00:39 +0100

    test was made

---
+++ client/src/components/goHomeButton/goHomeButton.test.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+
+import { GoHomeButton } from "./GoHomeButton";
+
+describe("should be..", () => {
+  test("should be the text in the dom", () => {
+    const { getByText } = render(
+      <BrowserRouter>
+        <GoHomeButton />
+      </BrowserRouter>
+    );
+    const searhedText = getByText(/back to search/i);
+    expect(searhedText).toHaveTextContent("back to search");
+  });
+});
