6204a2600f4e8d71a819fdde355612b0c5c1711c
Anhelina Lebedieva <a.lebedieva@godeltech.com>
2023-01-27 13:45:47 +0100

    Fetch try

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -173,6 +173,7 @@ class Index extends Component {
             options={filterOptions}
             onSubmit={(nextValues) => this.updateFilterValues(nextValues)}
           />
+          {console.log(filterOptions)}
         </Sidebar>
       </Page>
     );
