aebd1a9e42f44738215c4eb0774b49e067844d01
Vira Smulska <vera.smulskaya@gmail.com>
2023-01-31 13:20:42 +0100

    refactor component from class to function

--- client/src/components/features/features.jsx
+++ client/src/components/features/features.jsx
@@ -3,19 +3,15 @@ import Feature from "./__feature/__feature.jsx";
 import "./features.css";
 import { Subtitle } from "../subtitle/subtitle.jsx";
 
-export class Features extends Component {
-  render() {
-    const { features } = this.props;
-
-    return (
-      <section className="features">
-        <Subtitle>Features</Subtitle>
-        <ul className="features__list">
-          {Object.keys(features).map((key) => (
-            <Feature key={key} name={key} text={features[key]} />
-          ))}
-        </ul>
-      </section>
-    );
-  }
+export function Features({ features }) {
+  return (
+    <section className="features">
+      <Subtitle>Features</Subtitle>
+      <ul className="features__list">
+        {Object.keys(features).map((key) => (
+          <Feature key={key} name={key} text={features[key]} />
+        ))}
+      </ul>
+    </section>
+  );
 }
