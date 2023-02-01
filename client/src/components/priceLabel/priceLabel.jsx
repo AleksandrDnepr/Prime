e8ac92f03bda56a5e7618906b0a2578c062f7608
o.barashovets <o.barashovets@godeltech.com>
2023-02-01 18:30:24 +0100

    import component was removec

--- client/src/components/priceLabel/priceLabel.jsx
+++ client/src/components/priceLabel/priceLabel.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { ReactComponent as ApartmentSvg } from "./Apartment.svg";
 import { ReactComponent as TownHouseSvg } from "./townhouse.svg";
 
