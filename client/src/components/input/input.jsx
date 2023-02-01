6b2d08db017f6d00d8cb9e9e2e6f9d376a7f85a3
VERA <viraxyz@ukr.net>
2023-02-02 00:48:10 +0200

    fix onChange function

--- client/src/components/input/input.jsx
+++ client/src/components/input/input.jsx
@@ -1,80 +1,61 @@
-import React, { useState  } from "react";
+import React, { useState } from "react";
 import "./input.css";
 
+export default function Input({
+  type,
+  name,
+  placeholder,
+  size,
+  children,
+  min,
+  onChange,
+}) {
+  const [startValue, setStartValue] = useState();
+  const [currentValue, setCurrentValue] = useState();
 
-  
+  const changeValue = (e) => {
+    let value = e.target.value;
 
-  // function handleBlur ({ name, onChange, currentValue, startValue }) {
-    
-  //   if (currentValue !== startValue) {
-  //     onChange(name, currentValue);
-  //   }
-  // };
+    if (type === "number" && value !== "") {
+      value = Number(value);
+    }
 
-  export default function Input ({type, name, placeholder, size, children, min}) {
-    console.log('start input')
-    const [startValue, setStartValue] = useState();    
-    const [ currentValue, setCurrentValue ] = useState();
+    switch (e.type) {
+      case "focus":
+        setStartValue(value);
+        break;
 
-    const changeValue = (e, type) => { 
-        
-      let value = e.target.value;
-      console.log(value);
-      /* console.log(`type ${type}`); */
-  
-      if (type === "number" && value !== "") {
-        value = Number(value);
-      }
+      case "change":
+        setCurrentValue(value);
+        break;
 
-      console.log(e.type);
-  
-      switch (e.type) {
-        case "focus":
-          setStartValue(value);
-          
+      default:
+        return;
+    }
+  };
 
-          break;
-  
-        case "change":
-          console.log(`currentValue before ${currentValue}`)
-          console.log(`value ${value}`)
-          setCurrentValue(value);         
+  const handleBlur = () => {
+    if (currentValue !== startValue) {
+      onChange(name, currentValue);
+    }
+  };
 
-          console.log(`currentValue after ${currentValue}`)
-          break;
-  
-        default:
-          return;
-      }
-    };
-
-    const handleBlur = (name, currentValue, startValue) => {
-    
-      if (currentValue !== startValue) {
-        changeValue(name, currentValue);
-      }
-    };
-
-    console.log('end input')
-
-    return (
-      <label>
-        {children}
-        <input
-          type={type}
-          name={name}
-          id={name}
-          value={currentValue}
-          min={min}
-          placeholder={placeholder}
-          onChange={changeValue}
-          onFocus={changeValue}
-          onBlur={handleBlur}
-          autoComplete="off"
-          className={`input input_${size}`}
-        />
-      </label>
-    );
-    
-  }
-
+  return (
+    <label>
+      {children}
+      <input
+        type={type}
+        name={name}
+        id={name}
+        value={currentValue}
+        min={min}
+        placeholder={placeholder}
+        onChange={changeValue}
+        onFocus={changeValue}
+        onBlur={handleBlur}
+        autoComplete="off"
+        className={`input input_${size}`}
+      />
+    </label>
+  );
+}
